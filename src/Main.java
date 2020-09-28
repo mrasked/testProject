2cd6c4ce4e42d274c70e4b36c310536f60e1350c
mrasked <asked.cross@mail.ru>
2020-09-29 01:04:49 +0300

    Commit switch_case and commented

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+       /* Scanner numbercase = new Scanner(System.in);
+        switch (numbercase.nextInt()){
+        case 1:
+*/
         System.out.print("Введите число, что бы определить, является ли \n оно последовательностью числ Фибоначи : ");
         Scanner num= new Scanner(System.in);
         int a = 0, b = 1, c = 0, fibo;
@@ -19,6 +23,14 @@ public class Main {
         }
         if (c > fibo) {
             System.out.println("\n Число " + fibo + " не является число Фибоначи");
+
         }
+       /* case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            default:
+    }*/
     }
 }
