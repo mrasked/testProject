e8671f1858c8e030617a4c3719a6628820ffbc52
mrasked <asked.cross@mail.ru>
2020-09-29 20:15:54 +0300

    Last commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
        /* Scanner numbercase = new Scanner(System.in);
         switch (numbercase.nextInt()){
         case 1:
